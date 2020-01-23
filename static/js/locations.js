15d383a8e72be7bf0c3ed120a544a628888cb1a5
jabruhns <bruhnsjames@gmail.com>
2020-01-22 19:38:55 -0800

    html clean

---
+++ static/js/locations.js
@@ -0,0 +1,56 @@
+var locations = [{
+        Airport: "ATL",
+        LAT: 33.6407282,
+        LONG: -84.4277001
+    },
+    {
+        Airport: "LAX",
+        LAT: 33.943971,
+        LONG: -118.408324
+    },
+    {
+        Airport: "ORD",
+        LAT: 41.978928,
+        LONG: -87.905985
+    },
+    {
+        Airport: "DFW",
+        LAT: 32.8998091,
+        LONG: -97.0403352
+    },
+    {
+        Airport: "DEN",
+        LAT: 39.848794,
+        LONG: -104.672857
+    },
+    {
+        Airport: "JFK",
+        LAT: 40.641311,
+        LONG: -73.778139
+    },
+    {
+        Airport: "SFO",
+        LAT: 37.6213129,
+        LONG: -122.3789554
+    },
+    {
+        Airport: "SEA",
+        LAT: 47.4502499,
+        LONG: -122.3088165
+    },
+    {
+        Airport: "LAS",
+        LAT: 36.085359,
+        LONG: -115.147959
+    },
+    {
+        Airport: "MCO",
+        LAT: 28.432248,
+        LONG: -81.306538
+    },
+    {
+        Airport: "PDX",
+        LAT: 45.588654,
+        LONG: -122.593117
+    }
+]
--- static/js/logic.js
+++ static/js/logic.js
@@ -16,92 +16,9 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: API_KEY
 }).addTo(myMap);
 
-// Create a new marker
-// Pass in some initial options, and then add it to the map using the addTo method
-var marker = L.marker([33.6407282, -84.4277001], {
-    draggable: true,
-    title: "My First Marker"
-}).addTo(myMap);
-
-// Binding a pop-up to our marker
-marker.bindPopup("ATL");
-
-var marker = L.marker([33.943971, -118.408324], {
-    draggable: true,
-    title: "My Second Marker"
-}).addTo(myMap);
-
-// Binding a pop-up to our marker
-marker.bindPopup("LAX");
-
-var marker = L.marker([41.978928, -87.905985], {
-    draggable: true,
-    title: "My Third Marker"
-}).addTo(myMap);
-
-// Binding a pop-up to our marker
-marker.bindPopup("ORD");
-
-var marker = L.marker([32.8998091, -97.0403352], {
-    draggable: true,
-    title: "My Fourth Marker"
-}).addTo(myMap);
-
-// Binding a pop-up to our marker
-marker.bindPopup("DFW");
-
-var marker = L.marker([39.848794, -104.672857], {
-    draggable: true,
-    title: "My Fifth Marker"
-}).addTo(myMap);
-
-// Binding a pop-up to our marker
-marker.bindPopup("DEN");
-
-var marker = L.marker([40.641311, -73.778139], {
-    draggable: true,
-    title: "My Sixth Marker"
-}).addTo(myMap);
-
-// Binding a pop-up to our marker
-marker.bindPopup("JFK");
-
-var marker = L.marker([37.6213129, -122.3789554], {
-    draggable: true,
-    title: "My Seventh Marker"
-}).addTo(myMap);
-
-// Binding a pop-up to our marker
-marker.bindPopup("SFO");
-
-var marker = L.marker([47.4502499, -122.3088165], {
-    draggable: true,
-    title: "My Eighth Marker"
-}).addTo(myMap);
-
-// Binding a pop-up to our marker
-marker.bindPopup("SEA");
-
-var marker = L.marker([36.085359, -115.147959], {
-    draggable: true,
-    title: "My Ninth Marker"
-}).addTo(myMap);
-
-// Binding a pop-up to our marker
-marker.bindPopup("LAS");
-
-var marker = L.marker([28.432248, -81.306538], {
-    draggable: true,
-    title: "My Tenth Marker"
-}).addTo(myMap);
-
-// Binding a pop-up to our marker
-marker.bindPopup("MCO");
-
-var marker = L.marker([45.588654, -122.593117], {
-    draggable: true,
-    title: "My Eleventh Marker"
-}).addTo(myMap);
-
-// Binding a pop-up to our marker
-marker.bindPopup("PDX");
+for (var i = 0; i < locations.length; i++) {
+    L.marker([locations[i].LAT, locations[i].LONG], {
+        draggable: false,
+        title: locations[i].Airport
+    }).bindPopup(`${locations[i].Airport}`).addTo(myMap);
+}
