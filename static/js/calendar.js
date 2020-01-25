5ad4245d2a11f8b578e1dcdc08a9346b9b5c4770
jabruhns <bruhnsjames@gmail.com>
2020-01-24 18:28:53 -0800

    event

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,19 +1,19 @@
-function drawCalendar(dateData) {
-    console.log(dateData);
-    var weeksInMonth = function (month) {
+function drawCalendar(calResults) {
+    console.log(calResults);
+    var weeksInMonth = function(month) {
         var m = d3.timeMonth.floor(month)
         return d3.timeWeeks(d3.timeWeek.floor(m), d3.timeMonth.offset(m, 1)).length;
     }
 
-    var minDate = d3.min(dateData, function (d) { return new Date(d.day) })
-    var maxDate = d3.max(dateData, function (d) { return new Date(d.day) })
+    var minDate = d3.min(calResults, function(d) { return new Date(d.day) })
+    var maxDate = d3.max(calResults, function(d) { return new Date(d.day) })
 
     var cellMargin = 2,
         cellSize = 20;
 
     var day = d3.timeFormat("%w"),
         week = d3.timeFormat("%U"),
-        format = d3.timeFormat("%m-%d-%Y"),//format = d3.timeFormat("%Y-%m-%d"),// //format = d3.timeFormat("%Y-%m-%d"),
+        format = d3.timeFormat("%m-%d-%Y"), //format = d3.timeFormat("%Y-%m-%d"),// //format = d3.timeFormat("%Y-%m-%d"),
         titleFormat = d3.utcFormat("%a, %d-%b");
     monthName = d3.timeFormat("%B"),
         months = d3.timeMonth.range(d3.timeMonth.floor(minDate), maxDate);
@@ -23,7 +23,7 @@ function drawCalendar(dateData) {
         .enter().append("svg")
         .attr("class", "month")
         .attr("height", ((cellSize * 7) + (cellMargin * 8) + 20)) // the 20 is for the month labels
-        .attr("width", function (d) {
+        .attr("width", function(d) {
             var columns = weeksInMonth(d);
             return ((cellSize * columns) + (cellMargin * (columns + 1)));
             console.log(cellSize);
@@ -33,66 +33,67 @@ function drawCalendar(dateData) {
     svg.append("text")
         .attr("class", "month-name")
         .attr("y", (cellSize * 7) + (cellMargin * 8) + 15)
-        .attr("x", function (d) {
+        .attr("x", function(d) {
             var columns = weeksInMonth(d);
             return (((cellSize * columns) + (cellMargin * (columns + 1))) / 2);
         })
         .attr("text-anchor", "middle")
-        .text(function (d) { return monthName(d); })
+        .text(function(d) { return monthName(d); })
 
     var rect = svg.selectAll("rect.day")
-        .data(function (d, i) { return d3.timeDays(d, new Date(d.getFullYear(), d.getMonth() + 1, 1)); })
+        .data(function(d, i) { return d3.timeDays(d, new Date(d.getFullYear(), d.getMonth() + 1, 1)); })
         .enter().append("rect")
         .attr("class", "day")
         .attr("width", cellSize)
         .attr("height", cellSize)
         .attr("rx", 3).attr("ry", 3) // rounded corners
         .attr("fill", '#eaeaea') // default light grey fill
-        .attr("y", function (d) { return (day(d) * cellSize) + (day(d) * cellMargin) + cellMargin; })
-        .attr("x", function (d) { return ((week(d) - week(new Date(d.getFullYear(), d.getMonth(), 1))) * cellSize) + ((week(d) - week(new Date(d.getFullYear(), d.getMonth(), 1))) * cellMargin) + cellMargin; })
-        .on("mouseover", function (d) {
+        .attr("y", function(d) { return (day(d) * cellSize) + (day(d) * cellMargin) + cellMargin; })
+        .attr("x", function(d) { return ((week(d) - week(new Date(d.getFullYear(), d.getMonth(), 1))) * cellSize) + ((week(d) - week(new Date(d.getFullYear(), d.getMonth(), 1))) * cellMargin) + cellMargin; })
+        .on("mouseover", function(d) {
             d3.select(this).classed('hover', true);
         })
-        .on("mouseout", function (d) {
+        .on("mouseout", function(d) {
             d3.select(this).classed('hover', false);
         })
         .datum(format);
 
     rect.append("title")
-        .text(function (d) { return titleFormat(new Date(d)); });
+        .text(function(d) { return titleFormat(new Date(d)); });
 
     var lookup = d3.nest()
-        .key(function (d) { return d.day; })
-        .rollup(function (leaves) {
-            return d3.sum(leaves, function (d) { return parseInt(d.count); });
+        .key(function(d) { return d.day; })
+        .rollup(function(leaves) {
+            return d3.sum(leaves, function(d) { return parseInt(d.count); });
         })
-        .object(dateData);
+        .object(calResults);
 
     var scale = d3.scaleLinear()
-        .domain(d3.extent(dateData, function (d) { return parseInt(d.count); }))
+        .domain(d3.extent(calResults, function(d) { return parseInt(d.count); }))
         .range([0.4, 1]); // the interpolate used for color expects a number in the range [0,1] but i don't want the lightest part of the color scheme
 
-    rect.filter(function (d) { return d in lookup; })
-        .style("fill", function (d) { return d3.interpolateYlGn(scale(lookup[d])); })
+    rect.filter(function(d) { return d in lookup; })
+        .style("fill", function(d) { return d3.interpolateYlGn(scale(lookup[d])); })
         .select("title")
-        .text(function (d) { return titleFormat(new Date(d)) + ":  " + lookup[d]; });
+        .text(function(d) { return titleFormat(new Date(d)) + ":  " + lookup[d]; });
 
 }
 
 d3.json("Resources/JSON/flights.json").then((testData) => {
     //console.log(testData.data);
-    testData.data.forEach(function (data) {
+    testData.data.forEach(function(data) {
         var badSlash = "/";
         data.Date = data.Date.replace(badSlash, "-").replace(badSlash, "-");
         //console.log(data);
     });
-     function getPDXarrive(data) {
+
+    function getPDXarrive(data) {
         return data.Destination_Airport == "PDX" && data.Carrier_Code == "American Airlines";
     }
 
     var toPDX = testData.data.filter(getPDXarrive);
     //console.log(toPDX);
-    
+
 
     var dateString = toPDX.map(planes => planes.Date);
 
@@ -118,9 +119,9 @@ d3.json("Resources/JSON/flights.json").then((testData) => {
             day: uniqueDates[j],
             count: counter
         });
-    } 
+    }
     drawCalendar(dateCount);
     console.log(dateCount);
-}).catch(function (error) {
+}).catch(function(error) {
     console.log(error);
 });
--- static/js/logic.js
+++ static/js/logic.js
@@ -53,6 +53,16 @@ d3.select("#selDataset").on("change", function() {
     d3.event.preventDefault()
     var selectMenu = d3.select("#selDataset")
     var dataSelector = selectMenu.property('value')
-    d3.json(query).then((response) => {})
-    drawCalendar()
+    var calResults = d3.json(query).then((response) => {
+        var results = []
+        for (var i = 0; i < response.data.length; i++) {
+            if (response.data[i].Carrier_Code == dataSelector) {
+                results.push({
+                    day: response.data[i].Departure_Delay,
+                    count: response.data[i].Date
+                })
+            }
+        }
+    })
+    drawCalendar(calResults)
 })
